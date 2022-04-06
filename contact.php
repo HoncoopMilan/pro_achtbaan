a908b356346a7f27a02b585261c6896f58973e8e
MaykeKerssens <93595867+MaykeKerssens@users.noreply.github.com>
2022-04-06 10:07:37 +0200

    div wrapper en contact-block toegevoegd

--- contact.php
+++ contact.php
@@ -3,6 +3,7 @@
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="css/contact.css">
 	<title>Contact</title>
 </head>
 <body>
@@ -13,12 +14,15 @@
 	</header>
 	<main>
         <div class="wrapper">
-            <h2>Contact</h2>
-            <p>Maak contact met ons op via:</p>
-            <p>voorbeeld@email.com</p>
-            <p>+31 6 12345678</p>
-
-            <img src="img/hoofdkantoor.jpg" alt="Hoofdkantoor">
+			<div class="contact-block">
+				<h2>Contact</h2>
+				<p>Maak contact met ons op via:</p>
+				<p>voorbeeld@email.com</p>
+				<p>+31 6 12345678</p>
+			</div>
+			<div class="contact-block">
+				<img src="img/hoofdkantoor.jpg" alt="Hoofdkantoor">
+			</div>
 
         </div>\
         

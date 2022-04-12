fc830dda553c49a9052e9625bc771504ba957c53
Luna <93603497+TheLunaWitch@users.noreply.github.com>
2022-04-12 15:00:52 +0200

    Img voor metaal pagina toegevoegd

--- Metal.php
+++ Metal.php
@@ -3,6 +3,7 @@
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="css/metaal.css">
 	<title>SuperCoaster</title>
 </head>
 <body>
@@ -12,7 +13,21 @@
 		?>
 	</header>
 	<main>
+		<div class="main-menu">
+			<div class="wrapper">
+			<h2>Lorem Ipsum</h2>
+				<div class="text">
+					<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum voluptates deleniti, quis ea maiores atque quibusdam neque iste maxime perspiciatis laborum. 
+						Cupiditate culpa velit doloremque numquam consequatur ad iure Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo vel esse at animi suscipit quasi nemo laborum ea fuga itaque, nisi, nesciunt voluptates asperiores hic magni rerum minima obcaecati. Aut? Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eos excepturi eveniet eaque.
+						Nesciunt labore provident excepturi veritatis? Dolor consequatur corrupti numquam recusandae hic aperiam. Impedit distinctio sint quia ex error. Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur ratione necessitatibus qui aut nobis accusantium atque dolores cumque deleniti corporis incidunt, nemo excepturi debitis. Fuga quasi debitis cum asperiores delectus.</p>
+				</div>	
 
+				<div class="img">
+					<img src="img/Metal.png" alt="Metal">
+					<img src="img/Workers.png" alt="Workers">
+				</div>
+			</div>
+		</div>	
 	</main>
 	<footer>
 		<?php

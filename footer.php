69afa2efb42667cd66d0dd52cad7576763b052b9
HoncoopMilan <Honcoopmilan@gmail.com>
2022-04-19 10:01:28 +0200

    begin footer bijna af

---
+++ footer.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+<footer>
+	<p>&copy supercoasternv</p>
+	<a href="index.php">Home</a>
+	<a href="karretjes.php">Karretjes</a>
+	<a href="metal.php">Metaal</a>
+	<a href="contact.php">Contact</a>
+</footer>
+</html>
